4f99087c04d60c30f8bcc4acb1719f3aeea542ee
nuridavid <nurieldavid@gmail.com>
2021-03-10 21:46:06 -0500

    finished get requests and properly displayed on screen. need to build the put request and finish styling

--- sprint-one/scripts/index-page.js
+++ sprint-one/scripts/index-page.js
@@ -1,35 +1,3 @@
-// const url = "https://reqres.in/api/products/3";
-
-// axios
-//   .get(url)
-//   .then((res) => {
-//     console.log(res.data);
-//   })
-//   .catch((error) => {
-//     console.log("error");
-//   });
-
-const defaultComments = [
-  {
-    name: "Michael Lyons",
-    date: "12/8/2018",
-    comment:
-      "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-  },
-  {
-    name: "Gary Wong",
-    date: "12/12/2018",
-    comment:
-      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!",
-  },
-  {
-    name: "Theodore Duncan",
-    date: "11/15/2018",
-    comment:
-      "How can someone be so good!!! You can tell he ives for this and loves to do it every day. Everytime I see him i feel instantly happy! He's definitely my favorite ever!",
-  },
-];
-
 let form = document.getElementById("form");
 
 let comment = document.getElementById("form__comment");
@@ -37,27 +5,62 @@ let timestamp = document.getElementById("form__timestamp");
 let commentBox = document.querySelector(".comment__holder");
 let name = document.querySelector("#form__name");
 
-const comments = defaultComments
-  .map((comment) => {
-    return `<div class="commentLi">
-  <div class="pic__box">
-  <div class="pic"></div>
-  </div>
-  <div class="text">
-      <div class="top__comment">
-          <div class="comment__name">${comment.name}</div>
-          <div class="comment__date">${comment.date}</div>
-      </div>
-      <div class="commentLi__comment">${comment.comment}</div>
-  
-  </div>
+const key = "?api_key=3662ddd8-de29-457d-97d8-72c81d5722b4";
 
-  </div>
-  `;
+const url = "https://project-1-api.herokuapp.com/comments";
+let defaultComments;
+
+axios
+  .get(url + key)
+  .then((res) => {
+    console.log(res);
+    defaultComments = res.data;
+    let comments = defaultComments
+      .map((comment) => {
+        return `<div class="commentLi">
+       <div class="pic__box">
+         <div class="pic"></div>
+         </div>
+         <div class="text">
+            <div class="top__comment">
+                <div class="comment__name">${comment.name}</div>
+                <div class="comment__date">${comment.date}</div>
+            </div>
+            <div class="commentLi__comment">${comment.comment}</div>
+      
+         </div>
+      
+         </div>
+         `;
+      })
+      .join("");
+    commentBox.innerHTML = comments;
   })
-  .join("");
 
-commentBox.innerHTML = comments;
+  .catch((error) => {
+    console.log("error");
+  });
+
+// const comments = defaultComments
+//   .map((comment) => {
+//     return `<div class="commentLi">
+//   <div class="pic__box">
+//   <div class="pic"></div>
+//   </div>
+//   <div class="text">
+//       <div class="top__comment">
+//           <div class="comment__name">${comment.name}</div>
+//           <div class="comment__date">${comment.date}</div>
+//       </div>
+//       <div class="commentLi__comment">${comment.comment}</div>
+
+//   </div>
+
+//   </div>
+//   `;
+//   })
+//   .join("");
+
 // BREAK
 // BREAK
 // BREAK
@@ -94,61 +97,3 @@ form.addEventListener("submit", (event) => {
   commentBox.innerHTML = newComment + commentBox.innerHTML;
   form.reset();
 });
-
-// form.addEventListener("submit", submitComment);
-
-// function submitComment(event) {
-//   event.preventDefault();
-
-//   let objectName = {
-//     name: name.value,
-//     comment: comment.value,
-//   };
-
-//   console.log(objectName);
-
-//   defaultComments.push(objectName);
-//   // commentBox.innerHTML = objectName + commentBox.innerHTML;
-//   form.reset();
-// }
-
-// function displayComment(comObj) {
-//   console.log(defaultComments);
-//   // Re-add to the section element all comments from the comment array
-// }
-
-// const commentContainer = document.querySelector(".comment__container");
-// commentContainer.innerHTML = `<h1></h1>`;
-// function createComment(e) {
-//   let message = `<div><span class="top__comment"><h2> ${comment.value}</h2>
-//   </span>
-
-//   </div>`;
-
-//   const commentInput = e.target.comment.value;
-//   const nameInput = e.target.name.value;
-
-//   commentContainer.innerHTML = comment + commentContainer.innerHTML;
-// }
-
-// const SubmitTheForm = (event) => {
-//   event.preventDefault();
-//   createComment(commentInput);
-//   //   formSection.reset();
-// };
-
-// formSection.addEventListener("submit", SubmitTheForm);
-
-// const submitComment = (values) => {
-//   const commentSubmit = document.querySelector("#form__section");
-//   let commentBox = document.createElement("div");
-//   commentBox.className = "comment__box";
-
-// for (let i = 0; i < values.length; i++) {
-//   let commentText = document.createElement("p");
-//   commentBox.innerText = values[i];
-//   formSection.appendChild(commentText);
-// }
-//   commentSubmit.appendChild(commentBox);
-//   formSection.reset();
-// };
--- sprint-two-two/scripts/script.js
+++ sprint-two-two/scripts/script.js
@@ -1,62 +1,29 @@
 let tableSection = document.getElementById("tik");
 
-let table = [
-  {
-    date: "MON Dec 17 2018",
-    venue: "Ronald Lane",
-    location: "San Fransisco, CA",
-  },
+const key = "?api_key=3662ddd8-de29-457d-97d8-72c81d5722b4";
+let table;
+const url = "https://project-1-api.herokuapp.com/showdates";
 
-  {
-    date: "TUE Jul 18 2019",
-    venue: "Pier 3 East",
-    location: "San Fransisco, CA",
-  },
-
-  {
-    date: "FRI Jul 22 2019",
-    venue: "View Loungue",
-    location: "San Fransisco, CA",
-  },
-
-  {
-    date: "SAT Aug 12 2019",
-    venue: "Hyatt Agency",
-    location: "San Fransisco, CA",
-  },
-
-  {
-    date: "FRI Sep 05",
-    venue: "Moscow Center",
-    location: "San Fransisco, CA",
-  },
-  {
-    date: "FRI Sep 05",
-    venue: "Moscow Center",
-    location: "San Fransisco, CA",
-  },
-
-  {
-    date: "WED Aug 11 2019",
-    venue: "Pres Club",
-    location: "San Fransisco, CA",
-  },
-];
-
-let tableComplete = table
-  .map(
-    (row) => `<div class="shows__box">
+axios
+  .get(url + key)
+  .then((res) => {
+    table = res.data;
+    let tableComplete = table
+      .map(
+        (row) => `<div class="shows__box">
 <p>DATE</p>
       <div class="header">${row.date}</div>
       <p>VENUE</p>
-      <div>${row.venue}</div>
+      <div>${row.place}</div>
       <p>LOCATION</p>
       <div>${row.location}</div>
       <button>BUY TICKETS</button>
     </div>`
-  )
-  .join("");
-
-tableSection.innerHTML = tableComplete;
-
-// tableSection.append(table.innerHTML);
+      )
+      .join("");
+    tableSection.innerHTML = tableComplete;
+  })
+  .catch((error) => {
+    console.log("error");
+  });
+console.log();
